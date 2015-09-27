189a443160f241ba36499558990e72dc21c292e6
Vinzenz Sinapius <Vinzenz.Sinapius@googlemail.com>
2015-09-27 21:13:18 +0200

    Testing the sprite feature

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"log"
+
 	"github.com/vinzBad/grid"
 )
 
+func check(method string, err error) {
+	if err != nil {
+		log.Fatalln(method, "failed:", err)
+	}
+}
+
 type DwarfScene struct {
+	Alien grid.Sprite
+}
+
+func (s *DwarfScene) Setup() {
+	var err error
+	alienTx, err := grid.CreateTexture("alienGreen.png")
+	check("grid.CreateTexture", err)
+	s.Alien, err = grid.CreateSprite(alienTx)
+	check("grid.CreateSprite", err)
 }
 
 func (s *DwarfScene) Update(dt float32) {
@@ -12,20 +29,18 @@ func (s *DwarfScene) Update(dt float32) {
 }
 
 func (s *DwarfScene) Draw(dt float32) {
-
+	s.Alien.Draw()
 }
 
 func main() {
-	scene := DwarfScene{}
-
 	game := &grid.Game{
 		Width:      640,
 		Height:     480,
 		Fullscreen: false,
 		Resizable:  false,
 		Title:      "Dwarf",
-		Scene:      &scene,
+		Scene:      &DwarfScene{},
 	}
 
-	grid.Run(game)
+	check("grid.Run", grid.Run(game))
 }
